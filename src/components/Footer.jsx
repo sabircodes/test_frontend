919629c0bc1f8335170c73a7654c20298a2436cb
Sabir <93681489+sabircodes@users.noreply.github.com>
2024-03-25 16:21:20 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,7 @@ const Footer = () => {
   <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
     <div className="md:flex md:justify-between">
       <div className="mb-6 md:mb-0">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+        <a href="/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fabeeno</span>
         </a>
       </div>
@@ -17,10 +16,10 @@ const Footer = () => {
           <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact us</h2>
           <ul className="text-gray-500 dark:text-gray-400 font-medium">
             <li className="mb-4">
-              <a href="https://flowbite.com/" className="hover:underline">email: </a>
+              <a href="https://flowbite.com/" className="hover:underline">email: faiz_astral@hotmail.com</a>
             </li>
             <li>
-              <a href="https://tailwindcss.com/" className="hover:underline">Mob No. </a>
+              <a href="https://tailwindcss.com/" className="hover:underline">Mob No. 8800179168 </a>
             </li>
           </ul>
         </div>
